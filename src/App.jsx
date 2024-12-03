4c4286f57b3f8098b6eb7d3e20f21af1c6eec29f
Thanh <thanh.nguyen@beetech.com.vn>
2024-12-03 21:13:58 +0700

    con heo map

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home";
 function App() {
 
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/trang-chu" />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   );
